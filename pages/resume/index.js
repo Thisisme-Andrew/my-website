c7db1dc9ce4b14bba7e9ac58c18e8ba4131e1054
Andy Tang <andytang@Andys-Air.hitronhub.home>
2021-06-07 11:20:00 -0600

    resume link fix

--- pages/resume/index.js
+++ pages/resume/index.js
@@ -3,7 +3,7 @@ import styles from './index.module.css';
 const Resume = () => {
   return (
     <div className={styles.container}>
-      <img className={styles.resumeImage} src='https://lh3.googleusercontent.com/tBdlcy0Ec1od3Z59sYPf1QhGEbpxrsR7BqPpIhg-WAOHnJACfKU9GqZw8FhVxBuN7aYPfTK_PUvlTh0Kldv-IhL3OmdeqEsj1FRIRcuee5T776OtE1V1ZDgM9dIvPbpchFpZt1GhgQryhHgt2I8ZnOgD-0f7pNzVT2Bf1Td_tLl23fqWMiYv1C7F_FEmngGI9Y6LJp5VfzUFKajgzkB1DQ87yWgwc0CZl-dIfjTnBEl-wHdP0qmOHmMr4sqIJD7DapWBizNHRRKme_L4XOA6iBz171rdk41hn3l2O0rsH7awiGzf7MWgreYEAhI2hDsOJtqH5hWFAcFhuFJ2W3thlpY0ChMM1iOyPM6Ts8DihOMZawcm5Y8yevzZpdLJR3ZsLDS9YIOk1sg6fodJMRz3IrtUREP-jCmbMEs6LoosK7biFv5kYzMpNWX0WD1B5ElVN3mU0Pyx4YfE0XHyAxTp4iDD4qyFbW3CimMK7N4y4_5g_QYw7n-6XWXXRW-GwbPOR0lkeZnw8yWM2VRFadLVN9D2b06zk4YuDXhCAijR5-C1JOtyRqGHL4Xu_SUqj2kEGtenHbEmTdyaLVZZ2YJaNbbAJUdU0Lw0wiDoR9bR2pFCea6SUJQd9POsDTrmb9mvawNQKdetF95Q8I3n0WpkHd6vZ-Jjsg5106KxeBIwQ-UDy19V8PMERNHB0JxFsJ2FjmSATMSPa8rCvwO-4tLq1zDwzg=w1094-h1414-no?authuser=0'></img>
+      <img className={styles.resumeImage} src='https://cdn.shopify.com/s/files/1/0262/6653/3910/files/resume.jpg?v=1623086102'></img>
     </div>
   )
 }
